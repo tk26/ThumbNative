06778fc8a5e18a5c8dc19b9efb111e7f8c9e89a1
Tejas Kumthekar <tkumthekar@CHIC02TL4SFG8WN.sea.corp.expecn.com>
2018-01-29 20:19:41 -0600

    Remove react-native-camera and make app run on android device

--- AddProfilePicture.js
+++ AddProfilePicture.js
@@ -2,51 +2,13 @@ import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Text, Body, Button, View } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Camera from 'react-native-camera';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    preview: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        color: '#000',
-        padding: 10,
-        margin: 40
-    }
-});
-
-const initialState = {
-    imageUrl: 'test'
-};
 
 export default class AddProfilePicture extends Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-    }
-
-    takePicture() {
-        const options = {};
-        //options.location = ...
-        this.camera.capture({metadata: options})
-        .then((data) => console.log(data))
-        .catch(err => console.error(err));
-    }
-
     render() {
         return (
             <Container>
                 <Content style = { { padding: 20 } }>
-                    {/*<Card style = { { padding: 20 } }>
+                    <Card style = { { padding: 20 } }>
                         
                         <CardItem header style = { { alignSelf: 'center' } }>
                             <Icon name="user" size={100} />
@@ -70,24 +32,7 @@ export default class AddProfilePicture extends Component {
                                 </Text>
                             </Button>
                         </CardItem>
-                    </Card>*/}
-                    <View style={styles.container}>
-                        <Camera
-                            ref={(cam) => {
-                                this.camera = cam;
-                            }}
-                            style={styles.preview}
-                            aspect={Camera.constants.Aspect.stretch}
-                            CaptureTarget={Camera.constants.CaptureTarget.disk}
-                        >
-                            <Text style={styles.capture} onPress={this.takePicture.bind(this)}>
-                                [CAPTURE]
-                            </Text>
-                        </Camera>
-                        <Text>
-                            {this.path || this.state.imageUrl}
-                        </Text>
-                    </View>
+                    </Card>
                 </Content>
             </Container>
         );
--- android/app/src/main/java/com/thumbnative/MainApplication.java
+++ android/app/src/main/java/com/thumbnative/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new ReactNativeConfigPackage(),
-            new RCTCameraPackage(),
             new VectorIconsPackage()
       );
     }
