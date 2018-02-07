20dbf2da95acbb4ccdb1ca5f2c2800fef38ebca4
Tejas Kumthekar <tkumthekar@CHIC02TL4SFG8WN.sea.corp.expecn.com>
2018-02-06 19:09:22 -0600

    Add inviteUsers

--- Profile.js
+++ Profile.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
-import { Container, Content, View, Text, Button } from 'native-base';
+import { Container, Content, View, Text, Button, List, ListItem } from 'native-base';
 
 import { onSignOut } from './auth';
 
+var Contacts = require('react-native-contacts')
+
+const initialState = {
+    contacts: []
+}
+
 export default class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = initialState;
+    }
+
+    triggerInviteUsers() {
+        Contacts.getAll((err, contacts) => {
+            if(err === 'denied'){
+                // error
+            } else {
+                // contacts returned in []
+                this.setState({
+                    contacts: contacts.map(contact => { return {
+                        name: contact.givenName + " " + contact.familyName,
+                        phoneNumber: contact.phoneNumbers[0].number
+                    }})
+                });
+            }
+        });
+    }
+
     render() {
         return (
             <Container>
@@ -11,6 +38,12 @@ export default class Profile extends Component {
                     <Button rounded success style = { { alignSelf: 'center' } } onPress={() => this.props.navigation.navigate('ProfileProgress')}>
                         <Text>
                             Profile Progress
+                        </Text>
+                    </Button>
+
+                    <Button rounded success style = { { alignSelf: 'center' } } onPress={() => this.triggerInviteUsers()}>
+                        <Text>
+                            Invite Users
                         </Text>
                     </Button>
 
@@ -22,6 +55,26 @@ export default class Profile extends Component {
                         </Text>
                     </Button>
                 </Content>
+
+                { this.state.contacts.length > 0 ?
+                    <View>
+                        <Text>
+                            Contacts
+                        </Text>
+                        <List dataArray={this.state.contacts}
+                                renderRow={(contact) =>
+                                    <ListItem>
+                                        <Text>
+                                            {contact.name}
+                                            {'\n'}
+                                            {contact.phoneNumber}
+                                        </Text>
+                                    </ListItem>
+                                }>
+                            </List>
+                    </View>
+                : null }
+                
             </Container>
         );
     }
