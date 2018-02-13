4e695c8e19b47cb31bf47fce9c58a30ca14d897e
Tejas Kumthekar <tkumthekar@CHIC02TL4SFG8WN.sea.corp.expecn.com>
2018-02-12 20:46:09 -0600

    Integrate Invite Users with API endpoint

--- InviteUsers.js
+++ InviteUsers.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Content, View, Text, Button, List, ListItem, CheckBox, Body, Item } from 'native-base';
 import { FlatList } from 'react-native';
+import Config from 'react-native-config';
+import { Container, Content, View, Text, Button, List, ListItem, CheckBox, Body, Item } from 'native-base';
 
 var Contacts = require('react-native-contacts')
 
 const initialState = {
     contacts: [],
-    selectedChecklist: []
+    selectedChecklist: [],
+    validationResponse: '',
+    currentStep: 0,
 }
 
 export default class InviteUsers extends Component {
@@ -46,44 +49,101 @@ export default class InviteUsers extends Component {
         });
     }
 
+    handleErrors(response) {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response;
+    }
+
     sendInvites() {
-        alert(JSON.stringify(this.state.selectedChecklist));
+        let selectedContacts = this.state.contacts
+            .filter(contact => this.state.selectedChecklist.includes(contact.id))
+            .map(_contact => {
+                return {
+                    phone: _contact.phoneNumber.replace(/\D+/g, "").slice(-10),
+                    name: _contact.name,
+                }
+            });
+        fetch(Config.API_URL+'/user/invite/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "token" : global.auth_token,
+                "contactsInvited" : selectedContacts,
+            })
+        })
+        .then(this.handleErrors)
+        .then( () => {
+            this.setState({ 
+                validationResponse: "Invitations sent out successfully!",
+                currentStep: 1,
+            });
+        })
+        .catch( () => this.setState({ validationResponse: "Error: Cannot send out invitations" }) )
     }
 
     render() {
-        return (
-            <Container>
-                <Content>
-                    <View>
-                        <Text>
-                            Select one or more contacts and hit "Send"
-                        </Text>
-                        <Item>
-                            <FlatList
-                                extraData={this.state}
-                                keyExtractor={(item, index) => item.id}
-                                data={this.state.contacts}
-                                renderItem={({ item }) => {
-                                    return <ListItem>
-                                    <CheckBox
-                                        checked={this.state.selectedChecklist.includes(item.id) ? true : false}
-                                        onPress={() => this.onCheckBoxPress(item.id)}
-                                    />
-                                    <Body>
-                                        <Text>{item.name}</Text>
-                                    </Body>
-                                    </ListItem>
-                                }}
-                            />
-                        </Item>
-                        <Button rounded info onPress = { () => this.sendInvites() }>
+        if(this.state.currentStep === 0) {
+            return (
+                <Container>
+                    <Content>
+                        <View>
                             <Text>
-                                Send
+                                Select one or more contacts and hit "Send"
                             </Text>
-                        </Button>
-                    </View>
-                </Content>
-            </Container>
-        );
+                            <Item>
+                                <FlatList
+                                    extraData={this.state}
+                                    keyExtractor={(item, index) => item.id}
+                                    data={this.state.contacts}
+                                    renderItem={({ item }) => {
+                                        return <ListItem>
+                                        <CheckBox
+                                            checked={this.state.selectedChecklist.includes(item.id) ? true : false}
+                                            onPress={() => this.onCheckBoxPress(item.id)}
+                                        />
+                                        <Body>
+                                            <Text>
+                                                {item.name}
+                                                {'\n'}
+                                                {item.phoneNumber}
+                                            </Text>
+                                        </Body>
+                                        </ListItem>
+                                    }}
+                                />
+                            </Item>
+                            <Button rounded info onPress = { () => this.sendInvites() }>
+                                <Text>
+                                    Send
+                                </Text>
+                            </Button>
+                        </View>
+
+                        <View style = { { alignSelf: 'center' } }> 
+                            <Text>
+                                {this.state.validationResponse}
+                            </Text>
+                        </View>
+                    </Content>
+                </Container>
+            );
+        }
+        else {
+            return (
+                <Container>
+                    <Content>
+                        <View>
+                            <Text> 
+                                Invitations sent out successfully.
+                            </Text>
+                        </View>
+                    </Content>
+                </Container>
+            );
+        }
     }
 }
